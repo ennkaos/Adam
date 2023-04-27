79c8fc0b685d9905e3e5b96a8ef1b13c646224fb
Alexandru Florin Bucur <alexandru.f.bucur@avanade.com>
2023-04-27 15:04:30 +0200

    Schedule almost finieshed

--- src/app/home/row/row.component.ts
+++ src/app/home/row/row.component.ts
@@ -8,13 +8,18 @@ import { AppointmentModel } from 'src/app/models/AppointmentModel';
 })
 export class RowComponent {
   @Input() item: any = [];
+  @Input() groupsLength!: number;
   indexAr: number[] = [];
   ngOnInit(): void {
     console.log(this.item);
   }
   getClassOfNg(item: AppointmentModel): String {
-    if (item && item.groups === true && item.IsLab) {
+    if (item && item.groups === true && item.IsLab === false) {
       return 'LabGroups';
+    } else if (item && item.groups === true && item.IsLab === true) {
+      return 'LabGroupsTogheter';
+    } else if (item && item.groups === false && item.IsLab === true) {
+      return 'LabNotTogheter';
     } else {
       return 'cellNothing';
     }
